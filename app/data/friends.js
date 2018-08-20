7c4a477477089d2f6c6bfa6997f74a807c48df04
Tessa Williams <tessawilliams@Tessas-MacBook-Pro.local>
2018-08-19 23:50:55 -0600

    logic added

---
+++ app/data/friends.js
@@ -0,0 +1,52 @@
+var friendsList = [{
+    "name": "Donna Meagle",
+    "photo": "https://img.buzzfeed.com/buzzfeed-static/static/2014-01/campaign_images/webdr05/22/21/33-reasons-why-donna-meagle-should-be-your-hero-1-23479-1390443145-0_big.jpg",
+    "scores": [
+        3,
+        4,
+        2,
+        1,
+        4,
+        5,
+        3,
+        5,
+        1,
+        4
+    ]
+}, {
+    "name": "Ron Swanson",
+    "photo": "https://vignette.wikia.nocookie.net/manly/images/4/4b/Ron_swanson_mike_conley.jpg/revision/latest?cb=20130723170236",
+    "scores": [
+        5,
+        1,
+        4,
+        4,
+        5,
+        1,
+        2,
+        5,
+        4,
+        1
+    ]
+},
+
+{
+    "name": "Jean-Ralphio",
+    "photo": "https://i.kinja-img.com/gawker-media/image/upload/s--5YYJvJAW--/c_scale,f_auto,fl_progressive,q_80,w_800/qzcvfpnmqa02juvnjeka.jpg",
+    "scores": [
+        1,
+        3,
+        5,
+        4,
+        2,
+        3,
+        3,
+        1,
+        5,
+        2
+    ]
+}
+
+]
+
+module.exports = friendsList;
---
+++ app/routing/apiRoutes.js
@@ -0,0 +1,83 @@
+// LOAD DATA
+// We are linking our routes to a series of "data" sources which will hold arrays of information on possible friends.
+// 
+
+var friendsData = require("../data/friends");
+
+
+module.exports = function (app) {
+    // API GET Requests
+    // Below code handles when users "visit" a page.
+    // In each of the below cases when a user visits a link
+    // 
+
+    app.get("/api/friends", function (req, res) {
+        console.log(friendsData);
+        res.json(friendsData);
+
+    });
+
+    app.post("/api/friends", function (req, res) {
+    
+
+        var bestMatch = {
+            name: "",
+            photo: "",
+            friendDifference: 1000
+        };
+
+        // Take the result of the user"s survey POST and parse it.
+        var userInput = req.body;
+        var userScores = userInput.scores;
+
+        // This variable will calculate the difference between the user"s scores and the scores of
+        // each user in the database
+        var totalDifference = 0;
+
+        // Here we loop through all the friend possibilities in the database.
+        for (var i = 0; i < friendsData.length; i++) {
+
+            console.log(friendsData[i].name);
+            totalDifference = 0;
+
+            // Loop through all the scores of each friend
+            for (var j = 0; j < friendsData[i].scores[j]; j++) {
+
+                // Calculate the difference between the scores and sum them into the totalDifference
+                totalDifference += Math.abs(parseInt(userScores[j]) - parseInt(friendsData[i].scores[j]));
+
+                // If the sum of differences is less then the differences of the current "best match"
+                if (totalDifference <= bestMatch.friendDifference) {
+
+                    // Reset the bestMatch to be the new friend.
+                    bestMatch.name = friendsData[i].name;
+                    bestMatch.photo = friendsData[i].photo;
+                    bestMatch.friendDifference = totalDifference;
+                }
+            }
+        }
+
+        // Then save the user's data to the database (this has to happen AFTER the check. otherwise,
+        // the database will always return that the user is the user's best friend).
+        friendsData.push(userInput);
+
+        // Return a JSON with the user's bestMatch. This will be used by the HTML in the next page
+        res.json(bestMatch);
+
+
+
+        // console.log(friendsData);
+        friendsData.push(req.body);
+        // console.log(friendsData);
+        console.log(req.body);
+        res.json(friendsData);
+    });
+};
+
+///
+// var userResults = [];
+
+
+// for (var i = 0; i < friendsData.length; i++) {
+//     var friendSurvey = []
+// }
